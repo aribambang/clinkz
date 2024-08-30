c3456ecdbd8fe59945b61efbb4c1b78255d9a7d9
aribambang <aribk210@gmail.com>
2024-08-31 00:57:28 +0700

    linked list constructor

---
+++ 003 - linked lists/001-LinkedList.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+private:
+    int value;
+    Node *next;
+
+public:
+    Node(int value)
+    {
+        this->value = value;
+        next = nullptr;
+    }
+}
+
+class LinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int length;
+
+public:
+    LinkedList(int value)
+    {
+        Node *newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+}
