d634279f1027ae38d7c18426b348ac64ce233bda
aribambang <aribk210@gmail.com>
2024-09-10 13:19:00 +0700

    doubly linked list append

--- 004 - Doubly Linked Lists/001-DoublyLinkedList.cpp
+++ 004 - Doubly Linked Lists/001-DoublyLinkedList.cpp
@@ -33,6 +33,25 @@ public:
         length = 1;
     }
 
+    void append(int value)
+    {
+        Node *newNode = new Node(value);
+
+        if (length == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+
+        length++;
+    }
+
     void printList()
     {
         Node *temp = head;
@@ -62,5 +81,8 @@ public:
 int main()
 {
     DoublyLinkedList *dll = new DoublyLinkedList(1);
+
+    dll->append(2);
+
     dll->printList();
 }
