55035c7eff57817a3bd6e1bb042f4c6a2d68b474
aribambang <aribk210@gmail.com>
2024-09-10 12:17:16 +0700

    doubly linked list

---
+++ 004 - Doubly Linked Lists/001-DoublyLinkedList.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *prev;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->prev = nullptr;
+        this->next = nullptr;
+    }
+};
+
+class DoublyLinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int length;
+
+public:
+    DoublyLinkedList(int value)
+    {
+        Node *newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    void printList()
+    {
+        Node *temp = head;
+        while (temp)
+        {
+            cout << temp->value << endl;
+            temp = temp->next;
+        }
+    }
+
+    void getHead()
+    {
+        cout << "Head: " << head->value << endl;
+    }
+
+    void getTail()
+    {
+        cout << "Tail: " << tail->value << endl;
+    }
+
+    void getLength()
+    {
+        cout << "Length: " << length << endl;
+    }
+};
+
+int main()
+{
+    DoublyLinkedList *dll = new DoublyLinkedList(1);
+    dll->printList();
+}
